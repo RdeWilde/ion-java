11c4083b934f4fdc3a9f978e26be06e6716ab108
Robert de Wilde <email@robertdewilde.nl>
2016-12-16 22:49:33 +0100

    Fixed to references by making them final

--- examples/src/main/java/org/bitcoinj/examples/ForwardingService.java
+++ examples/src/main/java/org/bitcoinj/examples/ForwardingService.java
@@ -81,7 +81,7 @@ public class ForwardingService {
         // We want to know when we receive money.
         kit.wallet().addEventListener(new AbstractWalletEventListener() {
             @Override
-            public void onCoinsReceived(Wallet w, Transaction tx, Coin prevBalance, Coin newBalance) {
+            public void onCoinsReceived(Wallet w, final Transaction tx, Coin prevBalance, Coin newBalance) {
                 // Runs in the dedicated "user thread" (see bitcoinj docs for more info on this).
                 //
                 // The transaction "tx" can either be pending, or included into a block (we didn't see the broadcast).
